79454665650f335636ab1e2411ea4e29ad4ca616
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-07 20:15:15 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -14,11 +14,50 @@ public class Student {
     private double tuitionDue;
 
     /**
+     * A default constructor for the student object
+     */
+    public Student(){
+        this.profile = null;
+        this.creditHours = 0;
+    }
+
+    /**
+     * A parameterized constructor for the student object
+     * @param name Name of the student in string form
+     * @param major Major of the student in string form
+     * @param creditHours Credit hours the student is taking in integer form
+     */
+
+    public Student(String name, String major, int creditHours) throws IllegalArgumentException{
+        if(creditHours > 24 || creditHours < 3){
+            throw new IllegalArgumentException("Credit hours must be between 3 and 24!");
+        }
+        this.profile.setName(name);
+        this.profile.setMajor(major);
+        this.creditHours = creditHours;
+        if (creditHours >= 12){
+            this.isFullTime = true;
+        }
+        else{
+            this.isFullTime = false;
+        }
+    }
+
+    /**
      * The do-nothing method of calculating tuition.
      * The other sub-classes will have a method with the same name that will override this one.
      */
     public void tuitionDue(){}
 
+    /**
+     * A method lets a student pay their tuition
+     * @param amount integer value of how much the student is paying right now
+     */
+    public void payTuition(int amount, Date datePaid){
+        this.tuitionDue = this.tuitionDue - amount;
+        this.lastPaid = datePaid;
+        this.totalPayment = this.totalPayment + amount;
+    }
 
     /**
      * Converts the information of the student to a string format

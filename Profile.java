5a82f7f6da5197474b6a449ce898fbca4cefdf5f
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-07 20:14:44 -0400

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -6,6 +6,30 @@ public class Profile {
     private String name;
     private Major major; //5 majors and 2-character each: CS, IT, BA, EE, ME
 
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setMajor(String major){
+        if (major.equals("CS") || major.equals("Cs") || major.equals("cS") || major.equals("cs")){
+            this.major = Major.CS;
+        }
+        else if (major.equals("IT") || major.equals("It") || major.equals("iT") || major.equals("it")){
+            this.major = Major.IT;
+        }
+        else if (major.equals("BA") || major.equals("Ba") || major.equals("bA") || major.equals("ba")){
+            this.major = Major.BA;
+        }
+        else if (major.equals("EE") || major.equals("Ee") || major.equals("eE") || major.equals("ee")){
+            this.major = Major.EE;
+        }
+        else if (major.equals("ME") || major.equals("Me") || major.equals("mE") || major.equals("me")){
+            this.major = Major.ME;
+        }
+        else {
+            this.major = Major.Unknown;
+        }
+    }
     /**
      * Converts the information of the student to a string format
      * @return the student's profile in the form of a string

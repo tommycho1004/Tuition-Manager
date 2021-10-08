521973b0fb4fafa43334622f45120cea10635e51
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-08 13:56:12 -0400

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -6,10 +6,18 @@ public class Profile {
     private String name;
     private Major major; //5 majors and 2-character each: CS, IT, BA, EE, ME
 
+    /**
+     * A setter method to set the name of a student in their profile.
+     * @param name name of a student in String form
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * A setter method to set the major of a student in their profile.
+     * @param major major or a student in String form.
+     */
     public void setMajor(String major){
         if (major.equals("CS") || major.equals("Cs") || major.equals("cS") || major.equals("cs")){
             this.major = Major.CS;
@@ -36,7 +44,7 @@ public class Profile {
      */
     @Override
     public String toString(){
-
+        return this.name + ":" + this.major.majorString();
     }
 
     /**
@@ -46,6 +54,16 @@ public class Profile {
      */
     @Override
     public boolean equals(Object obj){
-
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof Profile) {
+            Profile newProf = (Profile) obj;
+            return newProf.name.equals(name) && newProf.major.equals(major);
+        }
+        return false;
     }
 }

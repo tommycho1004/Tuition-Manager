b42e9f43576aa4daffaa6a16dc1c65e4bf4a4892
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-10 11:25:45 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,144 +1,79 @@
 /**
- *  The Student superclass that stores the attributes of each student and their financial aid information.
- *  Use this class to populate attributes into a new Student object.
+ * The Resident subclass that extends from the Student superclass.
+ * Use this class to calculate the tuition due for a resident.
  * @author Tommy Cho, Neha Gudur
  */
+
 import java.text.DecimalFormat;
 
-public class Student {
-    private Profile profile;
-    private int creditHours;
-    private boolean isFullTime;
-    private Date lastPaid;
-    private double totalPayment;
-    private double tuitionDue = 0.0;
+public class Resident extends Student{
+    private boolean financialAid = false; //true if they received it, false if they didn't
+    private double financialAidAmount;
 
-    public static double universityFee = 3268.0;
-    public static double partTimeUniversityFee = 2614.4;
-    public static int extraCredits = 16;
-
-    public void setProfile(Profile profile){
-        this.profile = profile;
-    }
-    public void setCreditHours(int creditHours){
-        this.creditHours = creditHours;
-    }
-    /**
-     * A setter method for last paid date
-     * @param date Date of current payment
-     */
-    public void setLastPaid(Date date){
-        this.lastPaid = date;
-    }
+    private static double baseTuition = 12536.0;
+    private static double creditHourTuition = 404.0;
 
     /**
-     * A setter method for the total payment a student has made
-     * @param payment total payment in double form
-     */
-    public void setTotalPayment(double payment){
-        this.totalPayment = payment;
-    }
-
-    /**
-     * A setter method for the tuition due for a student
-     * @param tuition tuition due in double form
-     */
-    public void setTuitionDue(double tuition){
-        this.tuitionDue = tuition;
-    }
-
-    /**
-     * A getter method for a student's profile
-     * @return the student's profile in Profile object form
-     */
-    public Profile getProfile() {return profile;}
-    /**
-     * A getter method for the number of credit hours a student has
-     * @return credit hours in int form
-     */
-    public int getCreditHours(){
-        return creditHours;
-    }
-
-    /**
-     * A getter method for the fulltime status of a student
-     * @return
-     */
-    public boolean getIsFullTime(){
-        return isFullTime;
-    }
-
-    public Date getLastPaid(){
-        return lastPaid;
-    }
-
-    /**
-     * A getter method for the total payment a student has made
-     * @return the total payment in double form
-     */
-    public double getTotalPayment(){
-        return totalPayment;
-    }
-
-    /**
-     * A getter method for the tuition that a student is due
-     * @return tuition due in double form
-     */
-    public double getTuitionDue(){
-        return tuitionDue;
-    }
-
-    /**
-     * A default constructor for the student object
-     */
-    public Student(){
-        this.profile = null;
-        this.creditHours = 0;
-    }
-
-    /**
-     * A parameterized constructor for the student object
+     * Parameterized constructor for a resident that calls the super constructor.
      * @param name Name of the student in string form
      * @param major Major of the student in string form
+     * @param creditHours Credit hours the student is taking in int form
      */
-    public Student(String name, String major){
-        this.profile.setName(name);
-        this.profile.setMajor(major);
+    public Resident(String name, String major, int creditHours){
+        super(name, major, creditHours);
     }
 
     /**
-     * A parameterized constructor for the student object
-     * @param name Name of the student in string form
-     * @param major Major of the student in string form
-     * @param creditHours Credit hours the student is taking in integer form
+     * A method that gives a student a specified amount of financial aid and updates their tuition due
+     * @param amount amount of financial aid a student receives
      */
-    public Student(String name, String major, int creditHours){
-        this.profile.setName(name);
-        this.profile.setMajor(major);
-        this.creditHours = creditHours;
-        if (creditHours >= 12){
-            this.isFullTime = true;
+    public void receiveFinAid(double amount){
+        double newTuition = this.getTuitionDue() - amount;
+        this.setTuitionDue(newTuition);
+        this.financialAid = true;
+        this.financialAidAmount = amount;
+    }
+
+    private String finAidString(){
+        if (financialAid){
+            DecimalFormat dec1 = new DecimalFormat("#.00");
+            return "financial aid $" + dec1.format(this.financialAidAmount);
         }
         else{
-            this.isFullTime = false;
+            return "";
         }
     }
 
     /**
-     * The do-nothing method of calculating tuition.
-     * The other sub-classes will have a method with the same name that will override this one.
+     * The calculation method for the tuition of a resident student.
      */
-    public void tuitionDue(){}
-
-    /**
-     * A method lets a student pay their tuition
-     * @param amount integer value of how much the student is paying right now
-     */
-    public void payTuition(int amount, Date datePaid){
-        this.tuitionDue = this.tuitionDue - amount;
-        this.lastPaid = datePaid;
-        this.totalPayment = this.totalPayment + amount;
+    @Override
+    public void tuitionDue(){
+        double total = 0.0;
+        if (this.getTuitionDue() == 0 && this.getTotalPayment() == 0)
+        {
+            if (this.getIsFullTime()){
+                if(this.getCreditHours() <= extraCredits){
+                    //full time less than 16 credits calculation:
+                    total = baseTuition + universityFee;
+                }
+                else{
+                    //full time more than 16 credits calculation:
+                    total = baseTuition + universityFee + (this.getCreditHours() - extraCredits) * creditHourTuition;
+                }
+            }
+            else{
+                //part time calculation
+                total = this.getCreditHours() * creditHourTuition + partTimeUniversityFee;
+            }
+            this.setTuitionDue(total);
+        }
+        else{
+            return;
+        }
     }
+
+
 
     /**
      * Converts the information of the student to a string format
@@ -147,9 +82,8 @@ public class Student {
     @Override
     public String toString(){
         DecimalFormat dec = new DecimalFormat("#.00");
-        return this.profile.toString() + ":" + this.creditHours + " credit hours:tuition due:" +
-                dec.format(this.tuitionDue) + ":total payment:" + dec.format(this.totalPayment) +
-                ":last payment date:" + this.lastPaid.dateString() + ":student";
+        return this.getProfile().toString() + ":" + this.getCreditHours() + " credit hours:tuition due:" +
+                dec.format(this.getTuitionDue()) + ":total payment:" + dec.format(this.getTotalPayment()) +
+                ":last payment date:" + this.getLastPaid().dateString() + ":resident" + this.finAidString();
     }
-
 }

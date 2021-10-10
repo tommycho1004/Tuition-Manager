cf71ed681f22ff3afe79c4c694153435eb516d29
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-10 00:30:32 -0400

    Update TuitionManager.java

--- TuitionManager.java
+++ TuitionManager.java
@@ -11,6 +11,48 @@ import java.util.StringTokenizer;
 public class TuitionManager {
 
     /**
+     * A helper method to determine if a major is valid or not
+     * @param major Major of a student in string form
+     * @return true if the major is valid, false otherwise
+     */
+    public boolean majorChecker(String major){
+        if (major.equals("CS") || major.equals("Cs") || major.equals("cS") || major.equals("cs") ||
+                major.equals("IT") || major.equals("It") || major.equals("iT") || major.equals("it") ||
+                major.equals("BA") || major.equals("Ba") || major.equals("bA") || major.equals("ba") ||
+                major.equals("EE") || major.equals("Ee") || major.equals("eE") || major.equals("ee") ||
+                major.equals("ME") || major.equals("Me") || major.equals("mE") || major.equals("me")){
+            return true;
+        }
+        else{
+            System.out.println(major + "' is not a valid major.");
+            return false;
+        }
+    }
+
+    /**
+     * A helper method to determine whether the number of credit hours is valid or not
+     * @param creditHours credit hours a student is taking
+     * @return true if amount is valid, false otherwise
+     */
+    public boolean creditChecker(int creditHours){
+        if (creditHours < 0){
+            System.out.println("Credit hours cannot be negative.");
+            return false;
+        }
+        else if(creditHours < 3){
+            System.out.println("Minimum credit hours is 3.");
+            return false;
+        }
+        else if(creditHours > 24){
+            System.out.println("Credit hours exceed the maximum 24.");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    /**
      * A helper method that reads the input command and executes its respective function in the roster
      * @param st the string tokenizer with inputs from the user
      * @param roster the roster being modified
@@ -21,13 +63,30 @@ public class TuitionManager {
             if(st.countTokens() != 3){
                 System.out.println("Invalid number of parameters!");
             }
+            else{
+                String name = st.nextToken();
+                String major = st.nextToken();
+                int creditHours = Integer.parseInt(st.nextToken());
+                if(creditChecker(creditHours) && majorChecker(major)){
+                    Resident resident = new Resident(name, major, creditHours);
+                    roster.add(resident);
+                }
+            }
 
         }
         else if(command.equals("AN")){
             if(st.countTokens() != 3){
                 System.out.println("Invalid number of parameters!");
             }
-
+            else{
+                String name = st.nextToken();
+                String major = st.nextToken();
+                int creditHours = Integer.parseInt(st.nextToken());
+                if(creditChecker(creditHours) && majorChecker(major)){
+                    NonResident nonResident = new NonResident(name, major, creditHours);
+                    roster.add(nonResident);
+                }
+            }
         }
         else if(command.equals("AT")){
             if(st.countTokens() != 4){
@@ -90,7 +149,7 @@ public class TuitionManager {
         System.out.println("Tuition Manager starts running.");
         String input = reader.nextLine();
         StringTokenizer st = new StringTokenizer(input, ",");
-        Roster roster= new Roster();
+        Roster roster = new Roster();
         String command = st.nextToken();
         while(!command.equals("Q")) {
             if(command.equals("AR") || command.equals("AN") || command.equals("AT") || command.equals("AI") ||

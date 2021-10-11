b48b460625d4ca00e4a5bb3675ff3efcaef13144
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-10 21:59:24 -0400

    Update International.java

--- International.java
+++ International.java
@@ -1,13 +1,15 @@
+import java.text.DecimalFormat;
+
 /**
  * The International subclass that extends from the NonResident superclass.
  * Use this class to do get calculation information for international students.
+ *
  * @author Tommy Cho, Neha Gudur
  */
-import java.text.DecimalFormat;
-
-public class International extends NonResident{
+public class International extends NonResident {
     private boolean studyAbroad; //true if they are studying abroad, false otherwise
 
+    //static double additional fee for international students
     private static double additionalFee = 2650.0;
 
     /**
@@ -15,52 +17,46 @@ public class International extends NonResident{
      * This only sets it to true because the project description doesn't say a student can go from study-abroad to
      * non study-abroad.
      */
-    public void setStudyAbroad(){
-        if(this.getCreditHours() > 12){
+    public void setStudyAbroad() {
+        if (this.getCreditHours() > 12) {
             this.setCreditHours(12);
         }
         this.studyAbroad = true;
     }
 
     /**
-     * A getter method for study abroad status
-     * @return true if student is study abroad, false otherwise
-     */
-    public boolean getStudyAbroad(){
-        return studyAbroad;
-    }
-
-    /**
      * Parameterized constructor for an international student that calls the super constructor.
-     * @param name Name of the student in string form
+     *
+     * @param name  Name of the student in string form
      * @param major Major of the student in string form
      */
-    public International(String name, String major){
+    public International(String name, String major) {
         super(name, major);
     }
 
     /**
      * /**
      * Parameterized constructor for an international student that calls the super constructor.
-     * @param name Name of the student in string form
-     * @param major Major of the student in string form
+     *
+     * @param name        Name of the student in string form
+     * @param major       Major of the student in string form
      * @param creditHours Credit hours the student is taking in int form
      * @param studyAbroad True if they are study abroad, false otherwise
      */
-    public International(String name, String major, int creditHours, boolean studyAbroad){
+    public International(String name, String major, int creditHours, boolean studyAbroad) {
         super(name, major, creditHours);
         this.studyAbroad = studyAbroad;
     }
 
     /**
      * A helper method that returns whether a student is study abroad status or not in string form
+     *
      * @return "study abroad" if they are study abroad, nothing otherwise
      */
-    private String studyAbroadString(){
-        if (studyAbroad){
+    private String studyAbroadString() {
+        if (studyAbroad) {
             return ":study abroad";
-        }
-        else{
+        } else {
             return "";
         }
     }
@@ -69,42 +65,39 @@ public class International extends NonResident{
      * The calculation method for the tuition of a non-resident student.
      */
     @Override
-    public void tuitionDue(){
-        double total = 0.0;
-        if (this.getTuitionDue() == 0 && this.getTotalPayment() == 0)
-        {
-            if(!studyAbroad){
-                if (this.getCreditHours() <= extraCredits){
+    public void tuitionDue() {
+        double total;
+        if (this.getTuitionDue() == 0 && this.getTotalPayment() == 0) {
+            if (!studyAbroad) {
+                if (this.getCreditHours() <= extraCredits) {
                     //less than 16 hours calculation (internationals cannot be part time
                     total = baseTuition + universityFee + additionalFee;
-                }
-                else{
+                } else {
                     //more than 16 hours calculation
                     total = baseTuition + universityFee + additionalFee + (this.getCreditHours() - extraCredits) *
                             creditHourTuition;
                 }
-            }
-            else{
+            } else {
                 //if study abroad calculation
                 total = universityFee + additionalFee;
             }
             this.setTuitionDue(total);
         }
-        else{
-            return;
-        }
     }
 
     /**
      * Converts the information of the student to a string format
+     *
      * @return the student's profile in the form of a string
      */
     @Override
-    public String toString(){
+    public String toString() {
         DecimalFormat dec = new DecimalFormat("#0.00");
+        dec.setGroupingUsed(true);
+        dec.setGroupingSize(3);
         return this.getProfile().toString() + ":" + this.getCreditHours() + " credit hours:tuition due:" +
                 dec.format(this.getTuitionDue()) + ":total payment:" + dec.format(this.getTotalPayment()) +
-                ":last payment date:" + this.getLastPaid().dateString() + ":non-resident:international" +
+                ":last payment date: " + this.getLastPaid().dateString() + ":non-resident:international" +
                 this.studyAbroadString();
     }
 }

c68bffdcdc328f46436d93fdba615c6a11b142c4
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-10 21:59:34 -0400

    Update Major.java

--- Major.java
+++ Major.java
@@ -1,28 +1,29 @@
 /**
- *  This enum class defines the majors that a student can have.
+ * This enum class defines the majors that a student can have.
+ *
  * @author Tommy Cho, Neha Gudur
  */
 public enum Major {
     CS, IT, BA, EE, ME, Unknown;
-    public String majorString()
-    {
-        if (this == CS){
+
+    /**
+     * A method that converts a major object into a string
+     *
+     * @return major in string form
+     */
+    public String majorString() {
+        if (this == CS) {
             return "CS";
-        }
-        else if(this == IT){
+        } else if (this == IT) {
             return "IT";
-        }
-        else if(this == BA){
+        } else if (this == BA) {
             return "BA";
-        }
-        else if(this == EE){
+        } else if (this == EE) {
             return "EE";
-        }
-        else if(this == ME){
+        } else if (this == ME) {
             return "ME";
-        }
-        else{
-            return "--";
+        } else {
+            return "--"; //error case
         }
     }
 }
